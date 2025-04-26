c968a58397d60d95099aa573250505890d2aa618
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2025-04-26 22:27:18 +0300

    feat: implement remove()

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import path from 'node:path';
+import { existsSync } from 'node:fs';
+import { rm } from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(import.meta.dirname, 'files/fileToRemove.txt');
+
+    if (!existsSync(filePath)) throw new Error('FS operation failed');
+
+    rm(filePath);
 };
 
 await remove();
