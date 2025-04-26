025e614bb53c699fd04b19b56b26d893d46b80b7
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2025-04-26 14:31:19 +0300

    fix: get rid of hardcoded path

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,14 @@
+import path from 'node:path';
 import { existsSync } from 'node:fs';
 import { writeFile } from 'node:fs/promises';
 
 const create = async () => {
-    const path = './files/fresh.txt';
+    const url = path.join(import.meta.dirname, 'files/fresh.txt');
 
-    if (existsSync(path)) throw new Error('FS operation failed');
+    if (existsSync(url)) throw new Error('FS operation failed');
 
     const content = 'I am fresh and young';
-    writeFile(path, content);
+    writeFile(url, content);
 };
 
 await create();
