b7ecf9857a2f13e5696f4720b54b9c0f65423388
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2025-04-26 15:33:49 +0300

    feat: implement copy()

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import path from 'node:path';
+import { existsSync } from 'node:fs';
+import { cp } from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolderPath = path.join(import.meta.dirname, 'files');
+    const copyFolderPath = path.join(import.meta.dirname, 'files_copy');
+
+    if (!existsSync(sourceFolderPath) || existsSync(copyFolderPath)) throw new Error('FS operation failed');
+
+    cp(sourceFolderPath, copyFolderPath, { recursive: true });
 };
 
 await copy();
