7d1a40b8bcabb76907923770cedf44fd159d3b52
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2025-04-29 00:02:45 +0300

    feat: implement spawnChildProcess()

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import path from 'node:path';
+import { fork } from  'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = path.join(import.meta.dirname, 'files/script.js');
+
+    fork(filePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
