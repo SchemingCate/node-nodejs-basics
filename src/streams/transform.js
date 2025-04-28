0594f156feaadefc0b66322265bb03102e3b4214
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2025-04-28 17:34:10 +0300

    feat: implement transform()

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transStream = new Transform({
+        transform(chunk, _, callback) {
+            const chunkReversed = chunk
+                .toString()
+                .trim()
+                .split('')
+                .reverse()
+                .join('');
+
+            callback(null, chunkReversed + '\n');
+        }
+    })
+
+    pipeline(
+        process.stdin,
+        transStream,
+        process.stdout,
+        err => {
+            console.error(err);
+        }
+    );
 };
 
 await transform();
